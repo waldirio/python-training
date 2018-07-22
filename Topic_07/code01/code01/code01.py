2b6781b7c6cb8a91481fee3e6014c81b2b69a87a
Waldirio M Pinheiro <wpinheir@iroman.home>
2018-07-21 20:58:22 -0700

    Done

--- Topic_07/code01/code01/code01.py
+++ Topic_07/code01/code01/code01.py
@@ -125,7 +125,6 @@ def json_cidades():
 
 
 def json_city_state_full():
-    pass
     """
     Please implement here the code to read the Cidades.json and Estados.json, then 
     create the logic to generate the result as below
@@ -144,6 +143,75 @@ def json_city_state_full():
     $ 
     """
 
+    import json
+
+    # Block related to States
+    file_estado_content = open("code01/code01/Estados.json","r")
+    json_estado_content = json.load(file_estado_content)
+
+    estado_list = []
+
+    for each_name in json_estado_content:
+        new_id_estado = each_name['ID']+"-"+each_name['Sigla']
+        estado_list.append(new_id_estado)
+    # End block - States
+
+    # Block related to Cities
+    file_cidade_content = open("code01/code01/Cidades.json","r")
+    json_cidade_content = json.load(file_cidade_content)
+
+
+    # Print the total # of Cities
+    total_number_of_cidades = len(json_cidade_content)
+    print("Total Cities number is: {}".format(total_number_of_cidades))
+
+    # Print the total of Cities per State *at this moment use just the State code*
+    temp_city_list = []
+    temp_city_list_final = []
+    for each_name in json_cidade_content:
+        temp_city_list.append(each_name['Estado'])
+
+    # Temporary list with the State codes    
+    temp_city_list_final = list(set(temp_city_list))
+
+    # Initializing the counter to zero
+    count = 0
+    
+    # Loop using the Code State and for each one checking the complete State list just to count
+    # the number of entries per State
+    cidade_list = []
+    for each_city_code in temp_city_list_final:
+        for each_city_name in json_cidade_content:
+            if each_city_code == each_city_name['Estado']:
+                count = count + 1
+        new_id_cidade = each_city_code+"-"+str(count)
+        cidade_list.append(new_id_cidade)
+        # print("City code: {} -- # of Cities: {}".format(each_city_code,count))
+        count = 0
+
+
+    # List with all States code-Name *1-AC, 2-AL*
+    estado_list
+
+    # List with all Cities and code *1-22, 2-102*
+    cidade_list
+
+    print("State , # of Cities")
+    for estado in estado_list:
+        for cidade in cidade_list:
+            estado_code = estado.split("-")[0]
+            estado_name = estado.split("-")[1]
+            cidade_code = cidade.split("-")[0]
+            cidade_number = cidade.split("-")[1]
+            if estado_code == cidade_code:
+                print("{},{}".format(estado_name,cidade_number))
+
+
+
+
+    print("")
+
+
 if __name__ == "__main__":
     pass
     #Isimple_read_json()
@@ -151,4 +219,4 @@ if __name__ == "__main__":
     #formatted_json_output_with_count()
     #json_estados()
     #json_cidades()
-    #json_city_state_full()
+    # json_city_state_full()
